389f10761a18ccd0d147f8e9d37df5d47b51a8e8
YixueWang <yw1819@nyu.edu>
2015-05-11 14:58:01 -0400

    New json data for tree visualization.

--- processing/contains_what.py
+++ processing/contains_what.py
@@ -26,33 +26,32 @@ def read(file):
 	    		data.append(row)
 	data = np.asarray(data)
 
-	# data = data[1:5000,:]
-
-	feature = head[2:]
+	feature = head[1:]
 	feature_id = np.arange(len(feature))
 	feature_dct = dict(zip(feature,feature_id))
-	position_dct = dict(zip(feature_id,feature))
+
 	train = data[data[:,0]=='0',1:].astype(int)
 	test = data[data[:,0] =='1',1:].astype(int)
-	return train, test, feature_dct, position_dct
+	return train, test, feature_dct
 
 ### entropy id left right samples##
 ### criterion id impurity samples value
 
 def travel(dct, data, name_dct, indent=''):
 	ind = indent + '  '
-	# print store
-	# for k, v in dct.iteritems():
-		# if k == 'right':
 	if 'impurity' not in dct:
 		if 'left' in dct:
 			print ind + 'left' + dct['left']['id']
 			v = dct['left']
-			idx = name_dct[dct['rule'][:-10]]+1
+			idx = name_dct[dct['rule'][:-10]]
+			print dct['rule'][:-10]
+			print idx
 			to_left = data[data[:,idx]<0.5]
 			v['which'] = to_left[:,-1].tolist()
 			v['truth'] = to_left[:,1].tolist()
-			unique, counts = np.unique(to_left[:,1], return_counts=True)	
+			unique, counts = np.unique(to_left[:,0], return_counts=True)	
+			print unique,counts
+			# print v['truth']
 			if unique[0] == 1:
 				unique = np.insert(unique,0,0)
 				counts = np.insert(counts,0,0)
@@ -67,11 +66,12 @@ def travel(dct, data, name_dct, indent=''):
 		if 'right' in dct:
 			print ind + 'right' + dct['right']['id']
 			v = dct['right']
-			idx = name_dct[dct['rule'][:-10]]+1		
+			idx = name_dct[dct['rule'][:-10]]
 			to_right = data[data[:,idx]>=0.5]
 			v['which'] = to_right[:,-1].tolist()
 			v['truth'] = to_right[:,1].tolist()
-			unique, counts = np.unique(to_right[:,1], return_counts=True)	
+			unique, counts = np.unique(to_right[:,0], return_counts=True)	
+			print unique
 			if unique[0] == 1:
 				unique = np.insert(unique,0,0)
 				counts = np.insert(counts,0,0)
@@ -83,18 +83,14 @@ def travel(dct, data, name_dct, indent=''):
 				unique = np.append(unique,1)
 				v[unique[1]] = 0
 			dct['right'] = travel(v, to_right, name_dct, ind)
-	else:
-		dct['which'] = data[data[:,-1]].tolist()
-		# dct['truth']
-		print ind +'lllll'
 	return dct
 
 if __name__ == '__main__':
-	with open('ourTree.json') as data_file:    	
+	with open('tree_19_node.json') as data_file:    	
 		dct = json.load(data_file)
 
-	train,test, n_to_p_dct, p_to_n_dct= read('output.csv')
-	ytrain = train[:,1]
+	train, test, n_to_p_dct,= read('output.csv')
+	ytrain = train[:,0]
 
 	idd = np.arange(train.shape[0]).reshape(train.shape[0],1)
 
@@ -105,7 +101,8 @@ if __name__ == '__main__':
 	dct[unique[0]] = counts[0] 
 	dct[unique[1]] = counts[1]
  
+
 	d = travel(dct,train_with_id, n_to_p_dct)
-	with open('tree_contains.json', 'wb') as fp:
+	with open('20depth_all_data.json', 'wb') as fp:
 		json.dump(d, fp)
-	
+	# # 
