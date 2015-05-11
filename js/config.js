e466785c06afa03045dc6627751698bf2273194b
kennyazrina <kaniaazrina@gmail.com>
2015-05-11 08:44:30 -0400

    Edited config.js

--- js/config.js
+++ js/config.js
@@ -10,8 +10,8 @@ var env = "dev";
 	FILE
 ******************************************************/
 
-//var tree_file = (env === "dev") ? "data/tree_20depth.json" : "tree_contains_1.json";
-//var detailed_tree_file = (env === "dev") ? "data/tree_20depth_data.json" : "data/detailed_tree_huge.json";
+var tree_file = (env === "dev") ? "data/tree_20depth.json" : "tree_contains_1.json";
+var detailed_tree_file = (env === "dev") ? "data/tree_20depth_data.json" : "data/detailed_tree_huge.json";
 
-var tree_file = (env === "dev") ? "data/tree_small.json" : "tree_contains_1.json";
-var detailed_tree_file = (env === "dev") ? "data/detailed_tree_small.json" : "data/detailed_tree_huge.json";
+//var tree_file = (env === "dev") ? "data/tree_small.json" : "tree_contains_1.json";
+//var detailed_tree_file = (env === "dev") ? "data/detailed_tree_small.json" : "data/detailed_tree_huge.json";
